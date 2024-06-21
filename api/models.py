dd4c6c6497d001b9de06ff94c9b72118a4876d8b
hamidreza <takloohamidreza@gmail.com>
2024-06-21 23:54:37 +0330

    :sparkles: feat: Initialize models for TodoItem,TodoList and Note

---
+++ api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class TodoList(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
